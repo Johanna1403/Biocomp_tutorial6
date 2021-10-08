63b78ad7776a6ef1dd2dbf19dbe4715878941de6
Jolesk1403 <johannaolesk99@gmail.com>
2021-10-07 21:23:00 -0400

    Revert "First task completed: added pipeline code and the final file."
    This reverts commit afba54b10144724dbd151d619eea181e645f8cfa.

--- task1Code.sh
+++
@@ -1,7 +0,0 @@
-# This code takes the input file wages.csv and creates a file namedwages_sorted.txt
-# wages_sorted.txt contains gender and yearsExperience columns separated by space
-# the rows are sorted first by gender and then by yearsExperience
-
-# Usage: bash task1Code.sh InputFilename OutputFilename
-
-cat $1 | cut -d , -f 1,2 | tr -s "," " " | sort -V | uniq | grep -v "gender" > $2
